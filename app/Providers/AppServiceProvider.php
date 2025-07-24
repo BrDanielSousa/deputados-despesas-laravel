81c10bbaca58156ae8b5ecb56fc85d3e0e1964e9
unknown <danieloliveirasousaa@gmail.com>
2025-07-24 14:46:34 -0300

    feat: adicionar bindings de interfaces para implementações no AppServiceProvider para injeção de dependências

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,16 @@
 
 namespace App\Providers;
 
+use App\Clients\Contracts\DeputadoClientInterface;
 use Illuminate\Support\ServiceProvider;
+
+use App\Clients\DeputadoClient;
+use App\Repositories\Contracts\DeputadoRepositoryInterface;
+use App\Repositories\Contracts\DespesaRepositoryInterface;
+use App\Repositories\DeputadoRepository;
+use App\Repositories\DespesaRepository;
+use App\Services\Contracts\DeputadoServiceInterface;
+use App\Services\DeputadoService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(DeputadoClientInterface::class, DeputadoClient::class);
+        $this->app->bind(DeputadoRepositoryInterface::class, DeputadoRepository::class);
+        $this->app->bind(DespesaRepositoryInterface::class, DespesaRepository::class);
+        $this->app->bind(DeputadoServiceInterface::class, DeputadoService::class);
     }
 
     /**
